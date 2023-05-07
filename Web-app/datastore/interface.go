73eb3926fd692f66884bae37795a2985118c24c7
Forha-D <87822532+Forha-D@users.noreply.github.com>
2023-05-07 20:14:58 +0600

    Delete Web-app directory

--- Web-app/datastore/interface.go
+++
@@ -1,9 +0,0 @@
-package datastore
-
-import "github.com/Forha-D/Web-app/entities"
-
-type Animal interface {
-	Get(id int64) ([]entities.Animal, error)
-	Create (entities.Animal) (entities.Animal, error)
-}
-
